cf0df89e16dc65dc78931ebce0aa6931e0fced73
edwardbrefo <edwardbrefo185@gmail.com>
2026-03-10 16:54:39 +0000

    made a guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magicNum = random.Next(1, 101);  // 1 to 100
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+        
+        while (guess != magicNum)
+        {
+            if (guess > magicNum)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+        
+        Console.WriteLine("Congratulations! You guessed the magic number!");
     }
 }
