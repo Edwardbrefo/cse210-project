7907acd27dca07692f90a4429df0562cd2ac215e
edwardbrefo <edwardbrefo185@gmail.com>
2026-03-12 00:16:58 +0000

    made standalone functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        string name = PromptUserName();
+        int squareNum = SquareNumber();
+        DisplayResult(name, squareNum, squareNum * squareNum);
+    }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}! Nice to meet you.");
+        return name;
+    }
+
+    static int SquareNumber()
+    {
+        Console.Write("Please enter a number: ");
+        int squareNum = int.Parse(Console.ReadLine());
+        int result = squareNum * squareNum;
+        Console.WriteLine($"The square of {squareNum} is {result}");
+        return squareNum;
+    }
+
+    static void DisplayResult(string name, int squareNum, int result)
+    {
+        Console.WriteLine($"Hi {name}, the square of {squareNum} is {result}");
     }
 }
