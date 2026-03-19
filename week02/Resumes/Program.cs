c775a99a8174a0e4300befcc9ce645e0c1adb38d
edwardbrefo <edwardbrefo185@gmail.com>
2026-03-19 04:59:15 +0000

    created a resume using abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+          Job job1 = new Job();
+          job1._jobTitle = "Software Engineer";
+          job1._Company = "Microsoft";
+            job1._startYear = 2020;
+            job1._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name ="Edward Brefo";
+        myResume._jobs = new List<Job>();
+
+        myResume._jobs.Add(job1);
+       
+
+        myResume.Display();  
     }
 }
