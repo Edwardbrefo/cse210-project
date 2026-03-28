85764f01db975569463999e6610a24207753504a
edwardbrefo <edwardbrefo185@gmail.com>
2026-03-28 19:30:53 +0000

    made a scripture memorization app

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,111 @@
 using System;
 
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse, int endVerse = -1)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_endVerse != -1)
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+    }
+}
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public bool IsHidden
+    {
+        get { return _isHidden; }
+    }
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+    public void Show()
+    {
+        _isHidden = false;
+    }
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            return new string('_', _text.Length);
+        }
+        else
+        {
+            return _text;
+        }
+    }
+}
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        int hiddenCount = 0;
+        while (hiddenCount < numberToHide)
+        {
+            int index = random.Next(_words.Count);
+            if (!_words[index].IsHidden)
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+    public string GetDisplayText()
+    {
+        string wordsText = string.Join(" ", _words.Select(word => word.GetDisplayText()));
+        return $"{_reference.GetDisplayText()}: {wordsText}";
+    }
+    
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(word => word.IsHidden);
+    }
+
+
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+
     }
 }
